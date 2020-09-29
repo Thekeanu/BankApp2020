113caa3c962e20b54077c66b09cd342d0ca038ad
Thekeanu <keanu.piras@student.ehb.be>
2020-09-29 11:51:41 +0200

    het maken van de deposit en withdraw methods

--- bankingApp/BankLibrary/Account.cs
+++ bankingApp/BankLibrary/Account.cs
@@ -15,6 +15,36 @@ namespace BankLibrary
             string zin = $"IBAN: {IBAN} Balance: {Balance} Date created: {CreationDate} Intrest: {Intrest}.";
             return zin;
         }
+
+        public Account(string iban, decimal balance, DateTime creationDate, decimal intrest)
+        {
+            IBAN = iban;
+            Balance = balance;
+            CreationDate = creationDate;
+            Intrest = intrest;
+        }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Withdraw(decimal amount) {
+            if (amount > 0 && amount < Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            else { return false; }
+        }
     }
 
     
