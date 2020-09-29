24f39ec0f11076faa36bdff32fe2cda25b43be17
Thekeanu <keanu.piras@student.ehb.be>
2020-09-29 11:19:51 +0200

    setting up the project

---
+++ bankingApp/BankLibrary/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace BankLibrary
+{
+    public class Account
+    {
+        public string IBAN { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime CreationDate { get; set; }
+        public decimal Intrest { get; set; }
+
+        public override string ToString()
+        {
+            string zin = $"IBAN: {IBAN} Balance: {Balance} Date created: {CreationDate} Intrest: {Intrest}.";
+            return zin;
+        }
+    }
+
+    
+}
---
+++ bankingApp/bankingApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bankingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
